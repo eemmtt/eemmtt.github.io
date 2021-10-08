47bba3f1d0403b1b768737b65d1547d6ba56a02f
eemmtt <52972088+eemmtt@users.noreply.github.com>
2021-10-08 13:36:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,30 @@
 const agents = [];
 const copy = "Emmett deMuzio. I offer services: >> 2d/3d Design >> Animation >> Fabrication >> Programming Contact me via e-mail."
-const cell_x = 12;
-const cell_y = 16;
 const lineBreaks = [0,15,16,34,50,63,78,92,93,107];
+
+var cell_x = 0;
+var cell_y = 0;
 var speed = 0;
 var time = 0;
+var radius = 0;
+
+//mouse-tracking placement
+let offset_x;
+let offset_y;
+let ref;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cell_x = windowWidth * 2.5 / 100;
+  cell_y = windowHeight * 2.5 / 100;
+  
+  offset_x = cell_x * 19;
+  offset_y = cell_y * 9;
+  
+  textSize(cell_x);
+  radius = cell_x * 25;
+  
+  ref = createVector(windowWidth/4 + offset_x, windowHeight/2 + offset_y);
   
   for (let i = 0; i < lineBreaks.length; i++)
     {
@@ -27,15 +45,19 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+ 
+background(255);
+
+  
   if (time > 50 && speed < 1){
-    speed = speed + deltaTime/60000;
+    speed = speed + (deltaTime*deltaTime)/600000;
   } else {
     time = time + deltaTime/60;
   }
   
+  
   for (let a of agents) {
-    a.update(agents, speed);
+    a.update(agents, speed, radius, mouseIsPressed, ref);
     a.render(a);
     a.postdate();
   }
