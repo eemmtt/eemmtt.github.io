ddbae8e7a79c3fe183d010ca1190bb65fac56e28
eemmtt <52972088+eemmtt@users.noreply.github.com>
2021-10-08 13:36:43 -0400

    Update agent.js

--- agent.js
+++ agent.js
@@ -1,15 +1,14 @@
 class Agent {
   
   constructor(c, posX, posY, link) {
-    let rand = p5.Vector.random2D();
-    //this.position = createVector(random(windowWidth), random(windowHeight));
     this.position = createVector(posX, posY);
-    this.position_new = this.position;
+    this.position_new = createVector(posX, posY);
+    this.origin = createVector(posX, posY);
     this.velocity = p5.Vector.random2D();
-    this.velocity_new = this.velocity;
+    this.velocity_new = createVector(this.velocity.x, this.velocity.y);
     this.acceleration = createVector();
-    //this.character = this.getCharacter();
     this.character = c;
+    
     this.isLink = link;
     if (link) {
       this.link = createA("mailto: etmuzio@gmail.com", c);
@@ -34,20 +33,26 @@ class Agent {
     return dsq;
   }
 
-  update(agents,speed) {
+  update(agents,speed,radius, mouseDown, ref) {
     let total = 0;
     let average = createVector();
-    let boundsOffset = 6;
+    
 
     for (let a of agents) {
+      
+      if (a == this){
+        continue;
+      }
+      
       let d = this.dToroid(
         this.position.x,
         this.position.y,
         a.position.x,
         a.position.y
       );
+      
       //separation
-      if (a != this && d < 20 && d != 0) {
+      if (d < 0.2 * radius && d != 0) {
         let difference = p5.Vector.sub(this.position, a.position);
         difference.div(d);
         difference.mult(5);
@@ -55,13 +60,29 @@ class Agent {
         total++;
       }
       //cohesion
-      else if (a != this && d >= 20 && d < 100) {
+      else if (d >= 0.2 * radius && d < radius) {
         let difference = p5.Vector.sub(a.position, this.position);
         difference.mult(0.0001);
         average.add(difference);
         total++;
       }
+      
     }
+    
+    //origin seeking
+    if(mouseDown){
+      
+      let mouse = createVector(mouseX, mouseY);
+      let offset = p5.Vector.sub(mouse, ref);
+      let newOrigin = p5.Vector.add(this.origin, offset);
+      let difference = p5.Vector.sub(newOrigin, this.position);
+      let d = dist(newOrigin.x, newOrigin.y, this.position.x, this.position.y);
+      speed = (20*d)/windowWidth + 0.1;
+      difference.mult(10);
+      average.add(difference);
+      total++;
+    }
+    
     if (total != 0) {
       this.acceleration = average.div(total);
     }
@@ -71,8 +92,31 @@ class Agent {
     this.velocity_new.mult(speed);
     this.position_new = this.position.add(this.velocity_new);
 
-    //edge detection
+    this.edgeDetect();
+  }
 
+  render(agent) {
+    //strokeWeight(6);
+    //point(this.position.x, this.position.y);
+    
+    
+    if (!agent.isLink) {
+      text(agent.character, this.position_new.x, this.position_new.y);
+      //line(this.position_new.x, this.position_new.y, this.origin.x, this.origin.y);
+    } else {
+      agent.link.position(this.position_new.x, this.position_new.y);
+    }
+  }
+  
+  postdate() {
+    this.velocity = this.velocity_new;
+    this.position = this.position_new;
+  }
+  
+  edgeDetect(){
+    //edge detection
+    let boundsOffset = 8;
+    
     if (this.position_new.x > windowWidth + boundsOffset) {
       this.position_new.x = -boundsOffset;
     }
@@ -85,20 +129,5 @@ class Agent {
     if (this.position_new.y < -boundsOffset) {
       this.position_new.y = windowHeight + boundsOffset;
     }
-  }
-
-  render(agent) {
-    //strokeWeight(16);
-    //stroke(0);
-    //point(this.position.x, this.position.y);
-    if (!agent.isLink) {
-      text(agent.character, this.position_new.x, this.position_new.y);
-    } else {
-      agent.link.position(this.position_new.x, this.position_new.y);
-    }
-  }
-  postdate() {
-    this.velocity = this.velocity_new;
-    this.position = this.position_new;
   }
 }
